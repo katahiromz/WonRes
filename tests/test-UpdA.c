1356f248accce6b32f498bc6b351242864591257
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-05-07 10:30:12 +0900

    improve tests

--- tests/test-UpdA.c
+++ tests/test-UpdA.c
@@ -4,9 +4,19 @@
 
 int main(void)
 {
-    HANDLE hUpdate = BeginUpdateResourceA("test-Langs.exe", FALSE);
+    HANDLE hUpdate = WonBeginUpdateResourceA("test-Langs.exe", FALSE);
+    printf("hUpdate: %p\n", hUpdate);
     char sz[] = "This is a test";
-    UpdateResourceA(hUpdate, (LPSTR)RT_RCDATA, "Test", 0, sz, (DWORD)sizeof(sz));
-    EndUpdateResourceA(hUpdate, FALSE);
+    BOOL ret;
+    ret = WonUpdateResourceA(hUpdate, (LPSTR)RT_RCDATA, "Test", 0, sz, (DWORD)sizeof(sz));
+    printf("ret: %d\n", ret);
+    ret = WonEndUpdateResourceA(hUpdate, FALSE);
+    printf("ret: %d\n", ret);
+
+    HINSTANCE mod = LoadLibraryExA("test-Langs.exe", NULL, LOAD_LIBRARY_AS_DATAFILE);
+    HRSRC hRsrc = FindResourceExA(mod, (LPCSTR)RT_RCDATA, "Test", 0);
+    FreeLibrary(mod);
+    printf("hRsrc: %p\n", hRsrc);
+
     return 0;
 }
--- tests/test-UpdW.c
+++ tests/test-UpdW.c
@@ -4,9 +4,19 @@
 
 int main(void)
 {
-    HANDLE hUpdate = BeginUpdateResourceW(L"test-Langs.exe", FALSE);
+    HANDLE hUpdate = WonBeginUpdateResourceW(L"test-Langs.exe", FALSE);
+    printf("hUpdate: %p\n", hUpdate);
     char sz[] = "This is a test";
-    UpdateResourceW(hUpdate, (LPWSTR)RT_RCDATA, L"Test", 0, sz, (DWORD)sizeof(sz));
-    EndUpdateResourceW(hUpdate, FALSE);
+    BOOL ret;
+    ret = WonUpdateResourceW(hUpdate, (LPWSTR)RT_RCDATA, L"Test", 0, sz, (DWORD)sizeof(sz));
+    printf("ret: %d\n", ret);
+    ret = WonEndUpdateResourceW(hUpdate, FALSE);
+    printf("ret: %d\n", ret);
+
+    HINSTANCE mod = LoadLibraryExW(L"test-Langs.exe", NULL, LOAD_LIBRARY_AS_DATAFILE);
+    HRSRC hRsrc = FindResourceExW(mod, (LPCWSTR)RT_RCDATA, L"Test", 0);
+    FreeLibrary(mod);
+    printf("hRsrc: %p\n", hRsrc);
+
     return 0;
 }
